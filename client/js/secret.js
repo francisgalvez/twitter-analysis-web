a431aaad0e7b22e8bccc7f816089be7cab4dd853
Francis Gálvez <francisgalvezlao@gmail.com>
2019-05-17 18:00:18 +0200

    Añadido map.loader como módulo

--- client/js/secret.js
+++ client/js/secret.js
@@ -1,3 +1,3 @@
-var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NTgxMDc5ODF9.SmHFgrDjrHMY0ZzDT_nR5ZT2HflvdONc77BGcKb4hHY
+var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NTgxMDc5ODF9.SmHFgrDjrHMY0ZzDT_nR5ZT2HflvdONc77BGcKb4hHY'
 
 export { token };
