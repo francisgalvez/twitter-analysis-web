2acb7570403d434a1a9a18debf8c12d8ebcd8707
Francis Gálvez <francisgalvezlao@gmail.com>
2019-05-17 13:22:22 +0200

    Delete config.js

--- server/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    TOKEN_SECRET: process.env.TOKEN_SECRET || "francisgalvez",
-    MONGO_USER: 'root',
-    MONGO_PASSWORD: 'francisgalvez',
-    REDIS_PASSWORD: 'francisgalvez'
-};
