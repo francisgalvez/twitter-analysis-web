06e8faa49502cf25ca2b1022ca29e64ccc14c47a
Francis Gálvez <francisgalvezlao@gmail.com>
2019-05-21 13:43:05 +0200

    Actualización de contenidos en /apidocs

--- server/config.js
+++ server/config.js
@@ -1,6 +1,5 @@
 module.exports = {
     TOKEN_SECRET: process.env.TOKEN_SECRET || "francisgalvez",
     MONGO_USER: 'root',
-    MONGO_PASSWORD: 'francisgalvez',
-    REDIS_PASSWORD: 'francisgalvez'
+    MONGO_PASSWORD: 'francisgalvez'
 };
